0a17930d2217605b9718b7e78cde33a039a18211
Tieu Vu <dovanvu1792@gmail.com>
2023-11-16 09:24:32 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
     .binaryTarget(
         name: "HevSocks5Tunnel",
         url: "https://github.com/dovanvu1792/Tun2SocksKit/releases/download/Tag1/HevSocks5Tunnel.xcframework.zip",
-        checksum: "10f33ff41489607dde26d5a5cd1e5ddead92daf81206e04644cd5a40020e1881"
+        checksum: "20ea3348a50ca608faf046dcad64af576422cf1d39b04b51b0bacc086f1a0ce9"
     )
   ]
 )
